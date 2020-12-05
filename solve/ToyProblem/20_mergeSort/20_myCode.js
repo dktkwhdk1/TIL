d5b3ffa7501460f9981f4aed6218b64fd7828656
강희석 <heeseok@heeseok.local>
2020-12-06 01:15:51 +0900

    Toy problem 20

---
+++ solve/ToyProblem/20_mergeSort/20_myCode.js
@@ -0,0 +1,25 @@
+/* 20_mergeSort */
+
+function merge(left, right) {
+  let result = [];
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+  while (left.length) result.push(left.shift());
+  while (right.length) result.push(right.shift());
+  return result;
+}
+
+const mergeSort = function (arr) {
+  if (arr.length < 2) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = arr.slice(0, mid);
+  let right = arr.slice(mid, arr.length);
+  return merge(mergeSort(left), mergeSort(right));
+};
+
+console.log(mergeSort([5, 4, 3, 2, 1]));
