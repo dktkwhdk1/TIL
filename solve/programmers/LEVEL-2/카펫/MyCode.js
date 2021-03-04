e65c43877a2051f5ad2b206e7413ce0d4d70b1e8
강희석 <44192757+dktkwhdk1@users.noreply.github.com>
2021-03-04 13:48:39 +0900

    프로그래머스 카펫

--- solve/programmers/LEVEL-2/카펫/MyCode.js
+++ solve/programmers/LEVEL-2/카펫/MyCode.js
@@ -18,4 +18,25 @@ function solution(brown, yellow) {
 console.log(solution(10, 2)); // [4, 3]
 console.log(solution(8, 1)); // [3, 3]
 console.log(solution(24, 24)); // [8, 6]
-// https://programmers.co.kr/learn/courses/30/lessons/42842
+
+/**
+function solution(brown, yellow) {
+  for (let i = 3; i <= 2000; i++) {
+    for (let j = i; j >= 3; j--) {
+      let bCount = 0,
+        yCount = 0;
+      for (let r = 0; r < i; r++) {
+        for (let c = 0; c < j; c++) {
+          if (r === 0 || r === i - 1) {
+            bCount += 1;
+          } else {
+            if (c === 0 || c === j - 1) bCount += 1;
+            else yCount += 1;
+          }
+        }
+      }
+      if (bCount === brown && yCount === yellow) return [i, j];
+    }
+  }
+}
+ */
