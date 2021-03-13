48044b081ca1c6bd745dda2a61ca69701c426bac
강희석 <44192757+dktkwhdk1@users.noreply.github.com>
2021-03-13 17:27:42 +0900

    프로그래머스 방문 길이

--- solve/programmers/LEVEL-2/방문 길이/MyCode.js
+++ solve/programmers/LEVEL-2/방문 길이/MyCode.js
@@ -13,6 +13,62 @@ function solution(dirs) {
     }
   }
   let answer = 0;
+  let cy = 5,
+    cx = 5,
+    ny = 5,
+    nx = 5;
+
+  for (const dir of dirs) {
+    (cy = ny), (cx = nx);
+    switch (dir) {
+      case 'U':
+        ny = cy - 1;
+        break;
+      case 'D':
+        ny = cy + 1;
+        break;
+      case 'R':
+        nx = cx + 1;
+        break;
+      case 'L':
+        nx = cx - 1;
+        break;
+      default:
+        break;
+    }
+    if (ny < 0 || ny > 10 || nx < 0 || nx > 10) {
+      (ny = cy), (nx = cx);
+      continue;
+    }
+    if (!check[cy][cx][ny][nx]) {
+      check[cy][cx][ny][nx] = true;
+      check[ny][nx][cy][cx] = true;
+      answer += 1;
+    }
+  }
+  return answer;
+}
+
+console.log(solution('ULURRDLLU')); // 7
+console.log(solution('LULLLLLLU')); // 7
+console.log(solution('LLLLLLLLLLLLLLLLLLLLLL')); // 7
+
+/* 리팩토링 전 */
+function solution2(dirs) {
+  const check = [];
+  for (let i = 0; i < 11; i++) {
+    check[i] = [];
+    for (let j = 0; j < 11; j++) {
+      check[i][j] = [];
+      for (let r = 0; r < 11; r++) {
+        check[i][j][r] = [];
+        for (let c = 0; c < 11; c++) {
+          check[i][j][r][c] = false;
+        }
+      }
+    }
+  }
+  let answer = 0;
   let r = 5;
   let c = 5;
 
@@ -55,7 +111,4 @@ function solution(dirs) {
   return answer;
 }
 
-console.log(solution('ULURRDLLU')); // 7
-console.log(solution('LULLLLLLU')); // 7
-console.log(solution('LLLLLLLLLLLLLLLLLLLLLL')); // 7
 // https://programmers.co.kr/learn/courses/30/lessons/49994
