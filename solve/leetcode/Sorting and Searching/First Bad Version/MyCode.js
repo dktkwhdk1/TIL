ed03c6e9e4dec44396cef53fc962d8c0bc74cd6a
강희석 <44192757+dktkwhdk1@users.noreply.github.com>
2021-03-17 19:56:31 +0900

    leetcode First Bad Version

--- solve/leetcode/Sorting and Searching/First Bad Version/MyCode.js
+++ solve/leetcode/Sorting and Searching/First Bad Version/MyCode.js
@@ -17,5 +17,28 @@ var solution = function (isBadVersion) {
    * @param {integer} n Total versions
    * @return {integer} The first bad version
    */
-  return function (n) {};
+  // 이분탐색(Binary Search) 문제로 해석해서 풀었다.
+  return function (n) {
+    let start = 1,
+      end = n;
+
+    while (1) {
+      let mid = parseInt((start + end) / 2);
+
+      if (isBadVersion(mid)) {
+        if (!isBadVersion(mid - 1)) return mid;
+        end = mid;
+      } else {
+        if (isBadVersion(mid + 1)) return mid + 1;
+        start = mid;
+      }
+    }
+  };
 };
+
+console.log(
+  solution(function (n) {
+    if (n >= 4) return true;
+    return false;
+  })(5)
+); // 4
