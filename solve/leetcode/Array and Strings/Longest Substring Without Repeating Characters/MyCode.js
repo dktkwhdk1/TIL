36d77c4eb8f12c2407ecfd837dd66c0fe620b160
강희석 <44192757+dktkwhdk1@users.noreply.github.com>
2021-04-16 16:01:40 +0900

    leetcode Longest Substring Without Repeating Characters

--- solve/leetcode/Array and Strings/Longest Substring Without Repeating Characters/MyCode.js
+++ solve/leetcode/Array and Strings/Longest Substring Without Repeating Characters/MyCode.js
@@ -4,24 +4,23 @@
  */
 var lengthOfLongestSubstring = function (s) {
   if (s.length < 2) return s.length;
-
-  let current = 0;
-  let max = 0;
+  let current = 0,
+    max = 0;
   const obj = {};
 
   for (let i = 0; i < s.length; i++) {
-    if (obj[s[i]] == null) {
-      current += 1;
-    } else {
+    if (s[i] in obj) {
       current = Math.min(i - obj[s[i]], current + 1);
-    }
+    } else current += 1;
     max = Math.max(max, current);
     obj[s[i]] = i;
+    //console.log('obj : ', obj, 'max : ', max, 'current : ', current);
   }
   return max;
 };
 
 // test
+console.log(lengthOfLongestSubstring('abcdefghijklmnolpqrf')); // 15
 console.log(lengthOfLongestSubstring('abcabcbb')); // 3
 console.log(lengthOfLongestSubstring('bbbbb')); // 1
 console.log(lengthOfLongestSubstring('pwwkew')); // 3
