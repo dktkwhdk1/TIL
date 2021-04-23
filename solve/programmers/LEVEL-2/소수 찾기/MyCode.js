a72af117acefacb51c3987245e1b42d58fd2a697
강희석 <44192757+dktkwhdk1@users.noreply.github.com>
2021-04-23 16:58:28 +0900

    프로그래머스 소수 찾기

--- solve/programmers/LEVEL-2/소수 찾기/MyCode.js
+++ solve/programmers/LEVEL-2/소수 찾기/MyCode.js
@@ -42,3 +42,55 @@ function solution(numbers) {
 
 console.log(solution('17')); // 3
 console.log(solution('011')); // 2
+
+function solution2(numbers) {
+  const primes = getPrimeNumbers(numbers);
+
+  let check = Array(numbers.length).fill(false);
+  let answer = 0;
+  let map = new Map();
+
+  const result = [];
+  function pick(idx, max) {
+    if (idx === max) {
+      if (result[0] === '0') return;
+      const number = result.join('');
+      if (!primes[number] && !map.has(number)) {
+        map.set(number, true);
+        answer += 1;
+      }
+      return;
+    }
+    for (let i = 0; i < numbers.length; i++) {
+      if (!check[i]) {
+        check[i] = true;
+        result.push(numbers[i]);
+        pick(i + 1, max);
+        result.pop();
+        check[i] = false;
+      }
+    }
+  }
+  for (let i = 1; i <= numbers.length; i++) {
+    pick(0, i);
+    check = Array(numbers.length).fill(false);
+  }
+  return answer;
+}
+
+function getPrimeNumbers(numbers) {
+  const max = +numbers
+    .split('')
+    .map(val => +val)
+    .sort((a, b) => b - a)
+    .join('');
+  const primes = [true, true];
+
+  for (let i = 2; i <= max; i++) {
+    if (primes[i]) continue;
+    for (let j = 2 * i; j <= max; j += i) {
+      primes[j] = true;
+    }
+  }
+  return primes;
+}
